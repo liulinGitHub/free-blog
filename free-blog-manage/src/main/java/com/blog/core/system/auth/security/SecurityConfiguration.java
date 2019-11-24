4971408d434c38ce966797e23cb7452f02f49e19
liuin <liuin@51qqt.com>
2019-11-25 00:44:18 +0800

    添加后台系统权限模块

--- free-blog-manage/src/main/java/com/blog/core/system/auth/security/SecurityConfiguration.java
+++ free-blog-manage/src/main/java/com/blog/core/system/auth/security/SecurityConfiguration.java
@@ -1,10 +1,12 @@
 package com.blog.core.system.auth.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 /**
  * @ClassNmae: SecurityConfiguration
@@ -19,6 +21,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
+        http.authorizeRequests()
+            .withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>(){
+
+                @Override
+                public <O extends FilterSecurityInterceptor> O postProcess(O object) {
+                    object.setSecurityMetadataSource(null);
+                    object.setAccessDecisionManager(null);
+                    return null;
+                }
+            });
 
     }
 
